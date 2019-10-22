4d2472f6c15bb210f937cbbe71d2418efa8f9f45
Jonathan Hernandez <hernandezj84@gmail.com>
2019-10-22 14:49:56 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-calculator';
+	title = 'simple-calculator';
+	formula: string = ""; 
+	results: number = 0;
+	map_functions = {
+		'+': function (x, y) {return x + y},
+		'-': function (x, y) {return x - y},
+		'*': function (x, y) {return x * y},
+		'/': function (x, y) {return x / y}
+	}
+
+	operations = ["+", "-", "*", "/"];
+
+	constructor() {
+		//math_it_up['+'](1, 2) == 3;	
+	}
+
+
+	calculate(form: String) {
+		let i = form.length;
+		let simbol: String;
+		let newOperation: any;
+		while(i --) {
+			if (this.operations.includes(form.charAt(i))) {
+				simbol = form.charAt(i);
+			}
+		}
+
+		if (simbol) {
+			newOperation = form.split(`${simbol}`);
+			this.results = this.map_functions[`${simbol}`](Number(newOperation[0]), Number(newOperation[1]))
+		}
+		this.formula = "";
+	}
 }
